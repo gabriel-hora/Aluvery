b4551b1bd73cd57f0489799443f0a6b792f04c75
Gabri <gabrielhoradeveloper@gmail.com>
2022-11-21 22:10:36 -0300

    Implementa scroll em colunas e linhas

--- app/src/main/java/com/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -3,61 +3,187 @@ package com.example.aluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.aluvery.extensions.toBrazilianCurrency
+import com.example.aluvery.model.Product
 import com.example.aluvery.ui.theme.AluveryTheme
+import com.example.aluvery.ui.theme.Purple500
+import com.example.aluvery.ui.theme.Teal200
+import java.math.BigDecimal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AluveryTheme() {
-                Surface() {
-                    MyFirstComposable()
-                }
+            App()
+        }
+    }
+}
+
+@Composable
+fun App() {
+    AluveryTheme {
+        Surface {
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Spacer(Modifier)
+                ProductSection()
+                ProductSection()
+                ProductSection()
+                Spacer(Modifier)
             }
         }
     }
 }
 
+@Preview(showSystemUi = true)
 @Composable
-fun MyFirstComposable() {
-    Text(text = "Meu Primeiro Texto")
-    Text(text = "Meu segundo Texto Maior")
+fun AppPreview() {
+    App()
+}
+
+@Composable
+fun ProductItem(product: Product) {
+    Surface(
+        shape = RoundedCornerShape(size = 10.dp),
+        elevation = 4.dp,
+    )
+    {
+        Column(
+            Modifier
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple500,
+                                Teal200
+                            )
+                        )
+                    )
+                    .height(imageSize)
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = product.image),
+                    contentDescription = "Imagem do produto",
+                    Modifier
+                        .size(imageSize)
+                        .offset(y = imageSize / 2)
+                        .clip(shape = CircleShape)
+                        .align(BottomCenter),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Spacer(modifier = Modifier.height(imageSize / 2))
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = product.name,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = product.price.toBrazilianCurrency(),
+                    Modifier.padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun MyFirstComposablePreview() {
-    Column(
-        Modifier
-            .background(color = Color.Blue)
-            .padding(all = 24.dp)
-    ) {
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
-        Row(
-            modifier = Modifier
-                .padding(
-                    horizontal = 8.dp,
-                    vertical = 16.dp
-                )
-                .background(
-                    Color.Green
-                )
+fun ProductItemPreview() {
+    ProductItem(
+        Product(
+            name = LoremIpsum(50).values.first(),
+            price = BigDecimal("14.99"),
+            image = R.drawable.ic_launcher_background
         )
-        {
-            Text(text = "Texto 1")
-            Text(text = "Texto 2")
+    )
+}
+
+@Composable
+fun ProductSection() {
+    Column {
+        Text(
+            text = "Promoções",
+            Modifier.padding(
+                start = 16.dp,
+                end = 16.dp
+            ),
+            fontSize = 20.sp,
+            fontWeight = FontWeight(400)
+        )
+        Row(
+            Modifier
+                .padding(
+                    top = 8.dp,
+                )
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState()),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Spacer(Modifier)
+            ProductItem(
+                Product(
+                    name = "Pizza",
+                    price = BigDecimal("31.99"),
+                    image = R.drawable.pizza
+                )
+            )
+            ProductItem(
+                Product(
+                    name = "Hamburguer",
+                    price = BigDecimal("17.90"),
+                    image = R.drawable.hamburguer
+                )
+            )
+            ProductItem(
+                Product(
+                    name = "Coca-Cola",
+                    price = BigDecimal("5.90"),
+                    image = R.drawable.coca_cola
+                )
+            )
+            Spacer(Modifier)
         }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun ProductsSectionPreview() {
+    ProductSection()
+}
+
